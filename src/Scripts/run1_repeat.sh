2c706309c218d926e2be197c257cada9f11bab8f
u177183 <u177183@s001-n007.aidevcloud>
2023-03-18 12:45:35 -0700

    serial ifort results

---
+++ src/Scripts/run1_repeat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dir=ifort_serial/o0/march18
+
+for i in 1 2 3 4 5
+do
+./run1.sh 2>&1 | tee          ${dir}/out${i}.txt
+mv total_cell_mass_result.txt ${dir}/total_cell_mass_result${i}.txt
+mv total_pressure_result.txt  ${dir}/total_pressure_result${i}.txt
+mv total_sie_result.txt       ${dir}/total_sie_result${i}.txt
+mv velocity_result.txt        ${dir}/velocity_result${i}.txt
+mv runtime_150_1.txt          ${dir}/runtime_150_1_${i}.txt
+done
